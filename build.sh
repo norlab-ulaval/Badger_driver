3c06abfdfbeea5f272c2342b9678dc5276513f6e
al-bente <beren21@ulaval.ca>
2026-05-04 14:01:37 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-colcon build \
-        --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
-source ~/.bashrc
