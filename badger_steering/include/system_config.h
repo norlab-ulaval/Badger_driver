d2679d6fcda187ab0f138a075e6593100f3685d8
al-bente <renald.89@gmail.com>
2026-05-07 10:29:34 -0400

    Proper setup for potentiometer reading at 1kHz maximum sampling rate with interrupts

--- badger_steering/include/system_config.h
+++ badger_steering/include/system_config.h
@@ -3,13 +3,29 @@
 
 #include "stm32f3xx_hal.h"
 
-/* Exported peripheral handles */
-extern UART_HandleTypeDef huart2;
-extern ADC_HandleTypeDef hadc1;
 
-/* Exported functions */
+// Hardware handler librairies or functions
+extern UART_HandleTypeDef huart2;
+extern ADC_HandleTypeDef hadc2;
+extern TIM_HandleTypeDef htim6;
+
+// Hardware config declarations
 void SystemClock_Config(void);
+
 void MX_USART2_UART_Init(void);
 void MX_ADC1_Init(void);
+void MX_TIM6_Init(void);
 
-#endif // __SYSTEM_CONFIG_H
+
+// Interruption handling
+void SysTick_Handler(void);
+
+void TIM6_DAC_IRQHandler(void);
+void ADC1_2_IRQHandler(void);
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc);
+
+
+#endif /* __SYSTEM_CONFIG_H */
