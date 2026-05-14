6b6301d9d8ecdc33b9f7dd5ae02b5cfb2ed426c5
al-bente <renald.89@gmail.com>
2026-05-14 12:17:26 -0400

    adding the custom interface package

--- badger_steering/src/system_config.c
+++ badger_steering/src/system_config.c
@@ -56,7 +56,7 @@ void MX_ADC1_Init(void)
 
     hadc1.Instance = ADC1;
     hadc1.Init.ClockPrescaler       = ADC_CLOCK_SYNC_PCLK_DIV4;
-    hadc1.Init.Resolution           = ADC_RESOLUTION_12B;
+    hadc1.Init.Resolution           = ADC_RESOLUTION_8B;
     hadc1.Init.DataAlign            = ADC_DATAALIGN_RIGHT;
     hadc1.Init.ScanConvMode         = ADC_SCAN_DISABLE;
     hadc1.Init.EOCSelection         = ADC_EOC_SINGLE_CONV;
